ec6ad94fcbf0dba1da5c4731b0e8aab5488c2856
Antonyj26 <antonyjosu26@gmail.com>
2025-10-29 11:13:54 -0300

    feat(Techs): Integrate technician listing with API and remove mocked data

--- src/pages/MainPages/Techs.tsx
+++ src/pages/MainPages/Techs.tsx
@@ -1,31 +1,49 @@
 import { TechTable } from "../../components/TechTable/TechTable";
 import type { Tech } from "../../components/TechTable/TechTable";
-
-const techs: Tech[] = [
-  {
-    id: "1",
-    name: "Antony",
-    email: "antony@email.com",
-    availableHours: [
-      "08:00",
-      "09:00",
-      "10:00",
-      "11:00",
-      "12:00",
-      "13:00",
-      "14:00",
-      "15:00",
-    ],
-  },
-];
+import { useAuth } from "../../hooks/useAuth";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { AxiosError } from "axios";
+import { Loading } from "../../components/Loading/Loading";
 
 export function Techs() {
+  const { session } = useAuth();
+  const [tech, setTech] = useState<Tech[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchedTechs() {
+      try {
+        if (!session) {
+          return;
+        }
+
+        const response = await api.get("/admin/tech");
+        const data = response.data;
+
+        setTech(data.techs);
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          return alert(error.response?.data.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchedTechs();
+  }, [session]);
+
   const handleEdit = (techId: string) => {};
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <div>
       <h1 className="text-blue-dark font-bold text-xl mb-6">Técnicos</h1>
-      <TechTable onEdit={handleEdit} techs={techs} />
+      <TechTable onEdit={handleEdit} techs={tech} />
     </div>
   );
 }
